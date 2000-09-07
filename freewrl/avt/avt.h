a605d7f455382f23945a8f2611ccde0cbc8ab10e
rcoscali <rcoscali>
2000-09-07 23:40:33 +0000

    Add copy notice & headers ]ignoreAdde

--- freewrl/avt/avt.h
+++ freewrl/avt/avt.h
@@ -1,3 +1,30 @@
+/* $Id: avt.h,v 1.1.2.3 2000/09/07 23:40:33 rcoscali Exp $
+ * 
+ * -- avt.h --
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Public header for Average Load BTree implementation
+ *
+ * $Log: avt.h,v $
+ * Revision 1.1.2.3  2000/09/07 23:40:33  rcoscali
+ * Add copy notice & headers ]ignoreAdde
+ *
+ */
+
 #ifndef _AVT_H__
 #define _AVT_H__
 
