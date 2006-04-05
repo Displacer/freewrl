ba9b44dfba260e8f604db9d555c5dc286ddc445e
sdumoulin <sdumoulin>
2006-04-05 17:45:45 +0000

    Universal binary build

--- freewrl/VRMLC.pm
+++ freewrl/VRMLC.pm
@@ -1,4 +1,4 @@
-# $Id: VRMLC.pm,v 1.208 2006/03/09 20:43:33 crc_canada Exp $
+# $Id: VRMLC.pm,v 1.209 2006/04/05 17:49:40 sdumoulin Exp $
 #
 # Copyright (C) 1998 Tuomas J. Lukka 1999 John Stewart CRC Canada
 # Portions Copyright (C) 1998 Bernhard Reiter
@@ -8,6 +8,9 @@
 
 #
 # $Log: VRMLC.pm,v $
+# Revision 1.209  2006/04/05 17:49:40  sdumoulin
+# Universal binary build
+#
 # Revision 1.208  2006/03/09 20:43:33  crc_canada
 # Initial Event Utilities Component work.
 #
@@ -521,7 +524,7 @@ sub gen {
 
 /* OS X gives us a compile warning - hopefully this will cure it for all time */
 #ifdef AQUA
-struct tm {};
+//struct tm {};
 #endif
 
 /* Perl linking */
