f9882dc5e58e20a66d0b57059b811ac9f9349829
crc_canada <crc_canada>
2006-07-10 14:30:55 +0000

    semantic error fixed in previous version.

--- freewrl/VRMLC.pm
+++ freewrl/VRMLC.pm
@@ -1,4 +1,4 @@
-# $Id: VRMLC.pm,v 1.236 2006/07/10 14:24:11 crc_canada Exp $
+# $Id: VRMLC.pm,v 1.237 2006/07/10 14:30:55 crc_canada Exp $
 #
 # Copyright (C) 1998 Tuomas J. Lukka 1999 John Stewart CRC Canada
 # Portions Copyright (C) 1998 Bernhard Reiter
@@ -8,6 +8,9 @@
 
 #
 # $Log: VRMLC.pm,v $
+# Revision 1.237  2006/07/10 14:30:55  crc_canada
+# semantic error fixed in previous version.
+#
 # Revision 1.236  2006/07/10 14:24:11  crc_canada
 # add keywords for PROTO interface fields.
 #
@@ -645,7 +648,7 @@ sub gen {
 
 	#####################
 	# process PROTO keywords 
-	push @str, "\n/* Table of built-in PROTO keywords */\nextern const char *KEYWORDS[];\n";
+	push @str, "\n/* Table of built-in PROTO keywords */\nextern const char *PROTOKEYWORDS[];\n";
 	push @str, "extern const indexT PROTOKEYWORDS_COUNT;\n";
 
 	push @genFuncs1, "\n/* Table of PROTO keywords */\n       const char *PROTOKEYWORDS[] = {\n";
