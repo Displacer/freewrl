bd7e9ff04ab8798c6be0564e7ef87b322aa52cd7
crc_canada <crc_canada>
2008-06-17 19:00:26 +0000

    Geospatial work - June 17 2008

--- freewrl/VRMLRend.pm
+++ freewrl/VRMLRend.pm
@@ -4,7 +4,7 @@
 # See the GNU Library General Public License (file COPYING in the distribution)
 # for conditions of use and redistribution.
 #
-# $Id: VRMLRend.pm,v 1.217 2008/06/13 13:50:49 crc_canada Exp $
+# $Id: VRMLRend.pm,v 1.218 2008/06/17 19:00:27 crc_canada Exp $
 #
 # Name:        VRMLRend.c
 # Description:
@@ -17,6 +17,9 @@
 #              e.g. for #define glTexCoord2f(a,b) glTexCoord2f(a,b) see gen() [VRMLC.pm]
 #
 # $Log: VRMLRend.pm,v $
+# Revision 1.218  2008/06/17 19:00:27  crc_canada
+# Geospatial work - June 17 2008
+#
 # Revision 1.217  2008/06/13 13:50:49  crc_canada
 # Geospatial, SF/MFVec3d support.
 #
@@ -163,7 +166,7 @@
 	GeoLocation 		=>children,
 	GeoLOD 			=>children,
 	GeoMetadata		=>children,
-	GeoOrigin 		=>children,
+	GeoOrigin 		=>geoOrigin,
 	GeoPositionInterpolator	=>children,
 	GeoTouchSensor		=>children,
 	GeoViewpoint 		=>children,
@@ -351,6 +354,8 @@
 	MidiKey
 	PointLight
 	SpotLight
+	GeoLocation
+	GeoViewpoint
 /;
 
 #######################################################################
@@ -725,6 +730,7 @@
 # 
 
 %GEOSpatialKeywordC = map {($_=>1)} qw/
+	AA
 	AM
 	AN
 	BN
