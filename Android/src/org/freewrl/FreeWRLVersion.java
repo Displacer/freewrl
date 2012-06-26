d1dd853b3545296bca9bcbfcf75a5fe0abbe2eec
crc_canada <crc_canada>
2012-06-26 20:43:37 +0000

    Fixing up some debug messages and warnings on compile.

--- Android/src/org/freewrl/FreeWRLVersion.java
+++ Android/src/org/freewrl/FreeWRLVersion.java
@@ -1,5 +1,5 @@
 /*
-  $Id: FreeWRLVersion.java,v 1.7 2012/06/26 18:33:15 crc_canada Exp $
+  $Id: FreeWRLVersion.java,v 1.8 2012/06/26 20:43:37 crc_canada Exp $
 
 */
 
@@ -26,5 +26,5 @@ package org.freewrl;
 
 public class FreeWRLVersion {
 	public static String version = "1.22.13";
-	public static String compileDate = "2012-06-26:14:29:37";
+	public static String compileDate = "2012-06-26:14:50:02";
 }
