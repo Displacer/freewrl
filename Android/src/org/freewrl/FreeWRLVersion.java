6677ac5eb762e54e11778d876763aa20050ae4df
crc_canada <crc_canada>
2012-08-08 15:02:00 +0000

    Dave Joubert figured out how to get this running on Android 2.2; his changes are in here.

--- Android/src/org/freewrl/FreeWRLVersion.java
+++ Android/src/org/freewrl/FreeWRLVersion.java
@@ -1,5 +1,5 @@
 /*
-  $Id: FreeWRLVersion.java,v 1.14 2012/07/31 20:04:50 crc_canada Exp $
+  $Id: FreeWRLVersion.java,v 1.15 2012/08/08 15:02:00 crc_canada Exp $
 
 */
 
@@ -26,5 +26,5 @@ package org.freewrl;
 
 public class FreeWRLVersion {
 	public static String version = "1.22.13";
-	public static String compileDate = "2012-07-31:13:24:33";
+	public static String compileDate = "2012-08-08:11:01:03";
 }
