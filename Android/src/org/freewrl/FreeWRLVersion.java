8c1e81c2d06f6cabf329958e47f5b7146e8fc408
crc_canada <crc_canada>
2012-09-05 13:13:03 +0000

    Works on 2.2 to 4.x; had to put in a reset in InputStream for 2.2. Loads binary files (stl, or, eventually gzipped VRML) - Dave Joubert's fix for 2.2 only loaded ascii files.

--- Android/src/org/freewrl/FreeWRLVersion.java
+++ Android/src/org/freewrl/FreeWRLVersion.java
@@ -1,5 +1,5 @@
 /*
-  $Id: FreeWRLVersion.java,v 1.23 2012/09/04 02:56:47 crc_canada Exp $
+  $Id: FreeWRLVersion.java,v 1.24 2012/09/05 13:13:03 crc_canada Exp $
 
 */
 
@@ -26,5 +26,5 @@ package org.freewrl;
 
 public class FreeWRLVersion {
 	public static String version = "1.22.13";
-	public static String compileDate = "2012-09-03:22:52:04";
+	public static String compileDate = "2012-09-05:09:09:04";
 }
