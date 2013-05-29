f45398769b0b173b3c1c8ddcaf4191ca0eb79aac
dug9 <dug9>
2013-05-29 23:23:13 +0000

    dug9 for the testing fwl_keyPress0, I've reworked the linux code to be like win32, with a raw2,keypress1,raw3 keystroke combination.

--- freex3d/src/lib/scenegraph/Component_KeyDevice.c
+++ freex3d/src/lib/scenegraph/Component_KeyDevice.c
@@ -1,7 +1,7 @@
 /*
 =INSERT_TEMPLATE_HERE=
 
-$Id: Component_KeyDevice.c,v 1.26 2012/08/29 20:26:09 crc_canada Exp $
+$Id: Component_KeyDevice.c,v 1.27 2013/05/29 23:23:13 dug9 Exp $
 
 X3D Key Device Component
 
@@ -354,7 +354,7 @@ void sendKeyToKeySensor(const char key, int upDown) {
         /* make sure this has not been deleted  - we should really re-create list, but
          so few keySensor X3D nodes are in use, who cares? */
         if (checkNode(p->keySink[count],__FILE__,__LINE__)) {
-#ifdef _MSC_VER
+#ifndef AQUA //_MSC_VER
 		if (p->keySink[count]->_nodeType == NODE_KeySensor && (upDown != KeyChar)) sendToKS(p->keySink[count], (int)key&0xFFFF, upDown);
 #else
 		if (p->keySink[count]->_nodeType == NODE_KeySensor ) sendToKS(p->keySink[count], (int)key&0xFFFF, upDown);
