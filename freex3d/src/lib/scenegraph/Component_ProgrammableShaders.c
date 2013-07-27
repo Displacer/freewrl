bc4171a0749545a1dd59bdbdb44336fc55e07ae3
crc_canada <crc_canada>
2013-07-27 15:12:09 +0000

    more Shader node work - had some issues with non-shader Shape nodes, hopefully (close to) fixed

--- freex3d/src/lib/scenegraph/Component_ProgrammableShaders.c
+++ freex3d/src/lib/scenegraph/Component_ProgrammableShaders.c
@@ -1,7 +1,7 @@
 /*
 =INSERT_TEMPLATE_HERE=
 
-$Id: Component_ProgrammableShaders.c,v 1.61 2013/07/25 21:03:17 crc_canada Exp $
+$Id: Component_ProgrammableShaders.c,v 1.62 2013/07/27 15:12:09 crc_canada Exp $
 
 X3D Programmable Shaders Component
 
@@ -934,6 +934,7 @@ void compile_PackagedShader (struct X3D_PackagedShader *node) {
 
 /*****************************************************************/
 void render_ComposedShader (struct X3D_ComposedShader *node) {
+    ConsoleMessage("render_composedShader");
 		COMPILE_IF_REQUIRED
 		//JAS RUN_IF_VALID
 }
